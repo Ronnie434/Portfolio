859c3125673028ecb5c7be00568d0e82505b71ac
Ronnie434 <56054439+Ronnie434@users.noreply.github.com>
2020-02-15 18:23:26 -0800

    Animation Added

--- JS/main-min.js
+++ JS/main-min.js
@@ -5374,4 +5374,19 @@
       $(".navbar-burger").toggleClass("is-active"),
         $(".navbar-menu").toggleClass("is-active");
     });
+
+    if ($('.text-slider').length == 1) {
+      var typed_strings = $('.text-slider-items').text();
+         var typed = new Typed('.text-slider', {
+              strings: typed_strings.split(','),
+              typeSpeed: 80,
+              loop: true,
+              backDelay: 1100,
+              backSpeed: 30
+      });
+    }
+
   });
+
+ 
+
